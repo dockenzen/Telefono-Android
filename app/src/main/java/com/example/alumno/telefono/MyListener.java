eb387025bc9246bf6c1f183c2ce82ee98d1e949d
dockenzen <lea_eldoc@hotmail.com>
2017-03-23 21:58:19 -0300

    Clase 2

--- app/src/main/java/com/example/alumno/telefono/MyListener.java
+++ app/src/main/java/com/example/alumno/telefono/MyListener.java
@@ -2,6 +2,7 @@ package com.example.alumno.telefono;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by alumno on 23/03/2017.
@@ -11,6 +12,13 @@ public class MyListener implements View.OnClickListener
 {
     @Override
     public void onClick(View v) {
-        Log.d("Click","Se hizo click en el boton..");
+        if(v.getId() == R.id.boton1) {
+            Log.d("Click", "Se hizo click en el boton 1 ..");
+        }
+        if(v.getId() == R.id.boton2) {
+            Log.d("Click", "Se hizo click en el boton 2 ..");
+        }
+
+
     }
 }
