61488ab8bc94f9ec6985e6ed7b6ca03f836aa758
dockenzen <lea_eldoc@hotmail.com>
2017-03-23 21:33:53 -0300

    Clase 2

---
+++ app/src/main/java/com/example/alumno/telefono/Hola.java
@@ -0,0 +1,23 @@
+package com.example.alumno.telefono;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Hola extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola);
+        TextView txt = (TextView) findViewById(R.id.txtView);
+        txt.setText("Otro texto papa");
+
+        Button btn1 = (Button) findViewById(R.id.boton1);
+
+        MyListener lester = new MyListener();
+        btn1.setOnClickListener(lester);
+
+    }
+}
---
+++ app/src/main/java/com/example/alumno/telefono/MyListener.java
@@ -0,0 +1,16 @@
+package com.example.alumno.telefono;
+
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by alumno on 23/03/2017.
+ */
+
+public class MyListener implements View.OnClickListener
+{
+    @Override
+    public void onClick(View v) {
+        Log.d("Click","Se hizo click en el boton..");
+    }
+}
